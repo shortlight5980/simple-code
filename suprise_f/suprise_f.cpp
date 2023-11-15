b5fd84f83ccc76e9d9c50e078b5f0dfcad498a22
shortlight5980 <148292178+shortlight5980@users.noreply.github.com>
2023-11-15 21:05:20 +0800

    关机整蛊

---
+++ suprise_f/suprise_f.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char input[10] = { 0 };
+    system("shutdown -s -t 60");
+again:
+    printf("电脑将在1分钟内关机，如果你是小狗，请输入：“Yes”就取消关机！\n请输入>:");
+    scanf("%s", input);
+    if (0 == strcmp(input, "Yes"))
+    {
+        system("shutdown -a");
+        printf("取消成功，修勾:-)");
+    }
+    else
+    {
+        goto again;
+    }
+    system("pause");
+    return 0;
+}
